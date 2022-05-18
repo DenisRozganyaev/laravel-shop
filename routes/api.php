dbf017536f40e5665565d39864e8368aac57f508
DenisRozganyaev <denis.rozganyaev@gmail.com>
2022-05-18 20:00:45 +0300

    comment routes

--- routes/api.php
+++ routes/api.php
@@ -20,20 +20,20 @@ use Illuminate\Support\Facades\Route;
 
 //Route::post('login', \App\Http\Controllers\Api\AuthController::class)->name('login');
 
-Route::prefix('auth')->group(function() {
-    Route::post('login', [\App\Http\Controllers\Auth\AuthController::class, 'login'])->name('login');
-    Route::post('register', [\App\Http\Controllers\Auth\AuthController::class, 'register'])->name('register');
-
-    Route::middleware('auth:api')->group(function() {
-       Route::get('logout', [\App\Http\Controllers\Auth\AuthController::class, 'logout']);
-       Route::get('user', [\App\Http\Controllers\Auth\AuthController::class, 'user']);
-    });
-});
-
-Route::middleware(['api', 'auth:api', 'api_version:v1'])->prefix('v1')->group(function() {
-    require base_path('routes/api/api_v1.php');
-});
-
-Route::middleware(['api', 'api_version:v2'])->prefix('v2')->group(function() {
-    require base_path('routes/api/api_v2.php');
-});
+//Route::prefix('auth')->group(function() {
+//    Route::post('login', [\App\Http\Controllers\Auth\AuthController::class, 'login'])->name('login');
+//    Route::post('register', [\App\Http\Controllers\Auth\AuthController::class, 'register'])->name('register');
+//
+//    Route::middleware('auth:api')->group(function() {
+//       Route::get('logout', [\App\Http\Controllers\Auth\AuthController::class, 'logout']);
+//       Route::get('user', [\App\Http\Controllers\Auth\AuthController::class, 'user']);
+//    });
+//});
+//
+//Route::middleware(['api', 'auth:api', 'api_version:v1'])->prefix('v1')->group(function() {
+//    require base_path('routes/api/api_v1.php');
+//});
+//
+//Route::middleware(['api', 'api_version:v2'])->prefix('v2')->group(function() {
+//    require base_path('routes/api/api_v2.php');
+//});
